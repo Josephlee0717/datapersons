bc9001a7883fc796d09347611353c3bfe0143490
josephlee <josephlee@211.98.71.195>
2016-06-05 18:13:32 +0800

    dddddd

--- src/main/java/co/datapersons/zfb/Recorder.java
+++ src/main/java/co/datapersons/zfb/Recorder.java
@@ -1,4 +1,5 @@
 package co.datapersons.zfb;
+
 import java.io.InputStream;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
@@ -20,108 +21,123 @@ import co.datapersons.api.ApplicationContext;
 import co.datapersons.jdbc.JdbcDatabaseService;
 
 public class Recorder {
-	private static Logger logger = Logger.getLogger(UserAction.class); 
-	
-	public String saveDb(String extraCommon,String out_trade_no,String total_fee){
-//		Properties properties = new Properties();				
-//		InputStream in = this.getClass().getResourceAsStream("/sql.properties");
-//		HashMap<String, Object> result = null;
-		
+	private static Logger logger = Logger.getLogger(UserAction.class);
+
+	public String saveDb(String extraCommon, String out_trade_no, String total_fee) {
+		// Properties properties = new Properties();
+		// InputStream in =
+		// this.getClass().getResourceAsStream("/sql.properties");
+		// HashMap<String, Object> result = null;
+
 		String rtn = extraCommon;
 		int fistIdx = rtn.indexOf("@");
 		int secIdx = rtn.indexOf("#");
-		try{
-		String customerid = rtn.substring(0,fistIdx);
-		String shopid = rtn.substring(fistIdx + 1,secIdx);
-		String shopname = rtn.substring(secIdx + 1,rtn.length());
-			
-		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		String consumeTime = df.format(new Date()).toString();
-		//Ð´Êý¾Ý¿â
-		String[] queryParams = new String[]{"updateConsume",customerid,consumeTime,shopid,out_trade_no,"支付宝",total_fee};
-		JdbcDatabaseService jdbcService = ApplicationContext.getInstance().getJDBCService();
-		Object r = jdbcService.doService(queryParams);
-		String saveRtn = "Fail";
-		if(r!=null){			
-				saveRtn = "Success";
-				//根据paynumber + balance  来确定下一次的余额和 本次产生的QueueID
-				genQueueID(customerid,consumeTime,shopid,out_trade_no,shopname,total_fee);
-		}
-		//System.out.println("ZFB result=" + extraCommon +";customerid="+customerid+";shopid="+shopid+";shopname="+shopname+"; trade_no="+out_trade_no+"; total_fee="+total_fee+";saveRtn="+saveRtn);
-		logger.info("ZFB result=" + extraCommon +";customerid="+customerid+";shopid="+shopid+";shopname="+shopname+"; trade_no="+out_trade_no+"; total_fee="+total_fee+";saveRtn="+saveRtn);
-		}
-		catch(Exception ex){
-			logger.error("ZFB result=" + ex );
-		}
-		return "ZFB result=" + extraCommon + " has insert into db";	
-	}
-	
+		try {
+			String customerid = rtn.substring(0, fistIdx);
+			String shopid = rtn.substring(fistIdx + 1, secIdx);
+			String shopname = rtn.substring(secIdx + 1, rtn.length());
 
-	public void genQueueID(String customerid,String consumeTime,String shopid,String out_trade_no,String shopname,String total_fee){
+			SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			String consumeTime = df.format(new Date()).toString();
+			// Ð´Êý¾Ý¿â
+			String[] queryParams = new String[] { "updateConsume", customerid, consumeTime, shopid, out_trade_no, "支付宝",
+					total_fee };
+			JdbcDatabaseService jdbcService = ApplicationContext.getInstance().getJDBCService();
+			Object r = jdbcService.doService(queryParams);
+			String saveRtn = "Fail";
+			if (r != null) {
+				saveRtn = "Success";
+				// 根据paynumber + balance 来确定下一次的余额和 本次产生的QueueID
+				genQueueID(customerid, consumeTime, shopid, out_trade_no, shopname, total_fee);
+			}
+			// System.out.println("ZFB result=" + extraCommon
+			// +";customerid="+customerid+";shopid="+shopid+";shopname="+shopname+";
+			// trade_no="+out_trade_no+";
+			// total_fee="+total_fee+";saveRtn="+saveRtn);
+			logger.info("ZFB result=" + extraCommon + ";customerid=" + customerid + ";shopid=" + shopid + ";shopname="
+					+ shopname + "; trade_no=" + out_trade_no + "; total_fee=" + total_fee + ";saveRtn=" + saveRtn);
+		} catch (Exception ex) {
+			logger.error("ZFB result=" + ex);
+		}
+		return "ZFB result=" + extraCommon + " has insert into db";
+	}
+
+	public void genQueueID(String customerid, String consumeTime, String shopid, String out_trade_no, String shopname,
+			String total_fee) {
 		/*
-		 * Every time, the customer create a record of pay action, I get paynumber , and count of ID ,
-		 * Mak up ID list use userid , serial number and date ( userid + serial number + date).   
-		 * and input minimal queueID into lineup table ,input others into shalllineup table.
-		*/		
-		
-		double paynumber =Double.valueOf(total_fee);
-		String[] queryParams = new String[]{"GetUserBalance",customerid};
+		 * Every time, the customer create a record of pay action, I get
+		 * paynumber , and count of ID , Mak up ID list use userid , serial
+		 * number and date ( userid + serial number + date). and input minimal
+		 * queueID into lineup table ,input others into shalllineup table.
+		 */
+
+		double paynumber = Double.valueOf(total_fee);
+		String[] queryParams = new String[] { "GetUserBalance", customerid };
 		JdbcDatabaseService jdbcService = ApplicationContext.getInstance().getJDBCService();
 		Object r = jdbcService.doService(queryParams);
 		String sBalance = "";
-		if(r!=null){		
-			JSONArray result = (JSONArray)r;
-			if(result.size()>0){
-				JSONObject jsonResult = result.getJSONObject(0);
-				sBalance =  jsonResult.getString("balance");	
-			}
-		}
-		double balance = Double.valueOf(sBalance);
-		double balanceSum = 0 ;
+		double balanceSum = 0;
 		double dCount = 0;
-		if( balance == 0.00){
-			 dCount = Math.floor(paynumber/100);
-			 balanceSum = paynumber;
-		}else{
-			balanceSum = paynumber+balance;			
-			dCount = Math.floor(balanceSum/100);
-			balanceSum = ((paynumber%100)+balance)%100;
+		try {
+			if (r != null) {
+				JSONArray result = (JSONArray) r;
+				if (result.size() > 0) {
+					JSONObject jsonResult = result.getJSONObject(0);
+					sBalance = jsonResult.getString("balance");
+				}
+			}
+			double balance = Double.valueOf(sBalance);
+			balanceSum = paynumber + balance;
+			boolean linupFlag = true;
+			if (balanceSum > 100) {
+				dCount = Math.floor(balanceSum / 100);
+				balanceSum = balanceSum % 100;			
+			}else{
+				linupFlag = false;
+			} 
+			
+		} catch (Exception ex) {
+			logger.error("zfb result= GetUserBalance" + ex.getMessage());
 		}
-		
-		
-//		GetUserBalance
-		//Count of ID
-		
-		int s1 = (int)dCount;
+
+		// GetUserBalance
+		// Count of ID
+
+		int s1 = (int) dCount;
 		String userid = customerid;
 		Date dt = new Date();
-	    SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");   
-	    String today = sdf.format(dt);
-	    DecimalFormat df = new DecimalFormat("0000");
-	    
-	    queryParams = new String[]{"UpdateBalanceOfUserall",Double.toString(balanceSum),userid};				
-		r = jdbcService.doService(queryParams);
-		logger.info("UpdateBalanceOfUserall : balance=" + Double.toString(balanceSum) + " ; userid="+ userid);
-//		Map rtn = new HashMap();
-		for(int i = 0 ; i< s1 ; i++){
-//			rtn.put("key"+String.valueOf(i+1), userid+df.format(i+1).toString()+today);
-			String ID = userid+df.format(i+1).toString()+today;
-			if(i == 0 ){
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		String today = sdf.format(dt);
+		DecimalFormat df = new DecimalFormat("0000");
+		try {
+			queryParams = new String[] { "UpdateBalanceOfUserall", String.valueOf(balanceSum), userid };
+			r = jdbcService.doService(queryParams);
+			logger.info("UpdateBalanceOfUserall : balance=" + Double.toString(balanceSum) + " ; userid=" + userid);
+		} catch (Exception ex) {
+			logger.error("zfb result= UpdateBalanceOfUserall: balance = " + balanceSum +"; userid = "+userid+ ex.getMessage());
+		}
+		// Map rtn = new HashMap();
+		for (int i = 0; i < s1; i++) {
+			// rtn.put("key"+String.valueOf(i+1),
+			// userid+df.format(i+1).toString()+today);
+			String ID = userid + df.format(i + 1).toString() + today;
+			if (i == 0) {
 				String flag = "0";
-				queryParams = new String[]{"InsertIDIntoLineup",ID,today,flag,userid};				
+				queryParams = new String[] { "InsertIDIntoLineup", ID, today, flag, userid };
 				r = jdbcService.doService(queryParams);
-				logger.info("InsertIDIntoLineup : userid = " + ID + " ; date=" +today+" ; flag ="+flag+ " ; userid=" +userid);
-			}else{
+				logger.info("InsertIDIntoLineup : userid = " + ID + " ; date=" + today + " ; flag =" + flag
+						+ " ; userid=" + userid);
+			} else {
 				String flag = "0";
-				queryParams = new String[]{"InsertIDIntoShallLineup",ID,today,flag,userid};				
+				queryParams = new String[] { "InsertIDIntoShallLineup", ID, today, flag, userid };
 				r = jdbcService.doService(queryParams);
-				logger.info("InsertIDIntoShallLineup : userid = " + ID + " ; date=" +today+" ; flag ="+flag+ " ; userid=" +userid);
+				logger.info("InsertIDIntoShallLineup : userid = " + ID + " ; date=" + today + " ; flag =" + flag
+						+ " ; userid=" + userid);
 			}
 		}
-		
-		//input every QueueIDs into 
-//		return rtn.toString();
+
+		// input every QueueIDs into
+		// return rtn.toString();
 	}
-	
-	
+
 }
